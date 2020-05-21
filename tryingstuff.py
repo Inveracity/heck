d18246114f460938a206fd4193adf37bbf962c67
Christopher Baklid <christopherb@unity3d.com>
2020-05-21 20:04:33 +0200

    cleanup

--- tryingstuff.py
+++
@@ -1,7 +0,0 @@
-from game.password import encrypt
-from game.password import decrypt
-
-encrypted = encrypt("bacon", "r25KjzgbsKiOUdD7")
-
-print(encrypted)
-print(decrypt(encrypted, "r25KjzgbsKiOUdD7"))
